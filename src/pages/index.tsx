86b94ddb307abc0e2c8bc84cdd9c765cdbdc7d3f
Iago Luan <iago.luancj@gmail.com>
2024-08-27 20:28:45 -0300

    Correção deploy - $ inserido nas props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,10 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import SupaProvider from "../Context/context";
 
-import NavBar from "../components/Nav/NavBar";
-import Main from "../components/Main/index"
+
 import { NextUIProvider } from '@nextui-org/react'
+import NavBar from "@/components/Nav/NavBar";
+import Main from "@/components/Main/index";
 
 export default function Home() {
 
