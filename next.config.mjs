b4102508bd119e1e5e50d0c39259a62e98989d7f
Iago Luan <iago.luancj@gmail.com>
2024-08-27 20:31:13 -0300

    Correção deploy - $ inserido nas props

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
