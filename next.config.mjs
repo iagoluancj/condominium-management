ae4efdc4cb752838552055813fbf2a9884ba0e86
Iago Luan <iago.luancj@gmail.com>
2024-08-27 20:35:29 -0300

    Correção deploy - $ inserido nas props

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
