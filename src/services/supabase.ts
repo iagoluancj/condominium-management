1e9813d8ac8ca064dda394a14b8fe4bfb194dec6
Iago Luan <iago.luancj@gmail.com>
2024-08-27 22:07:31 -0300

    agora vai

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://wletlhsjwcdvwxjqkrae.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsZXRsaHNqd2Nkdnd4anFrcmFlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNDIwMzgwNCwiZXhwIjoyMDM5Nzc5ODA0fQ.HMyFRMBGlri7WTCa2PEubzgZCs6FvfoNbythOrzPdN0'
 
 export const supabase = createClient(supabaseUrl!, supabaseKey!)
