f92254f2ce169c9288cafb2f4e345c0082189861
Iago Luan <iago.luancj@gmail.com>
2024-08-27 21:30:39 -0300

    agora vai

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+import path from 'path';
 
+const __dirname = new URL('.', import.meta.url).pathname;
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+    return config;
+  },
+};
 export default nextConfig;
