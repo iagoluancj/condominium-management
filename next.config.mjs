74205418f36a6f9e72090ef029cb2cac4a38d959
Iago Luan <iago.luancj@gmail.com>
2024-08-27 21:24:13 -0300

    ta dificil viu

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
 };
 
 export default nextConfig;
