eeac93ec6f0a4f7cd9da52f4ff518fe59ba5035e
Iago Luan <iago.luancj@gmail.com>
2024-09-27 22:50:38 -0300

    Teste pages vercel

---
+++ src/pages/Telas/index.tsx
@@ -0,0 +1,16 @@
+// import { MainInquilinos, NavBar } from "@/Component";
+// import FooterCM from "@/Component/Footer";
+// import SupaProvider from "@/Context/context";
+
+// export default function Telas() {
+
+//   return (
+//     <>
+//       <SupaProvider>
+//         <NavBar></NavBar>
+//         <MainInquilinos></MainInquilinos>
+//         <FooterCM></FooterCM>
+//       </SupaProvider>
+//     </>
+//   );
+// }
